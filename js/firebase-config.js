dd65436484822577b9870adde1f193b651e35a1e
AdissPl <49759054+AdissPl@users.noreply.github.com>
2025-08-10 15:15:53 +0200

    Update firebase-config.js

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -1,19 +1,12 @@
-// js/firebase-config.js
-import { initializeApp } from "https://www.gstatic.com/firebasejs/12.1.0/firebase-app.js";
-import { getAuth, onAuthStateChanged } from "https://www.gstatic.com/firebasejs/12.1.0/firebase-auth.js";
-
-export const firebaseConfig = {
+// js/firebase-config.js (compat style for SG.fb.init())
+// This file replaces js/firebase-config.sample.js in your HTML files.
+window.FB_CONF = {
   apiKey: "AIzaSyCANGRhbT_a_BTkxWeLl2yfqYIbR3safoU",
   authDomain: "stajnia-gier.firebaseapp.com",
   projectId: "stajnia-gier",
   storageBucket: "stajnia-gier.firebasestorage.app",
   messagingSenderId: "510649052386",
   appId: "1:510649052386:web:8dde595ded00986da182bb",
-  measurementId: "G-J8DH8VC9JL"
+  measurementId: "G-J8DH8VC9JL",
+  OWNER_UID: "rl8kUYGmACafnTW1JbDhmmVNKEo2"
 };
-
-export const app  = initializeApp(firebaseConfig);
-export const auth = getAuth(app);
-
-// Helper
-export function onReady(cb){ onAuthStateChanged(auth, u => cb(u||null)); }
